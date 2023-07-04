00ba5fe49ce77eb577fbb38d4751b438bd6f322f
Jakub Wawrzyński <jacobwawrzynski@gmail.com>
2023-07-04 22:42:12 +0200

    Add GenreEnum, modify MovieDto

--- VioRentals.Web/DTOs/MovieDto.cs
+++ VioRentals.Web/DTOs/MovieDto.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public DateTime DateAdded { get; set; }
+        public DateTime DateAdded { get; set; } = DateTime.Now;
         public DateTime? ReleaseDate { get; set; }
         public byte NumberInStock { get; set; }
         public byte NumberAvailable { get; set; }
