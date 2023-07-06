e847a689f9a8e8680794e1101653acb1f0c25a95
KrzysztofJaronczyk <krzysztofjaronczykzyt@gmail.com>
2023-07-06 19:21:34 +0200

    Update .bat startup file. Now works on non absolute path.

--- start.bat
+++ start.bat
@@ -1,9 +1,6 @@
-@echo off
-ROCKET_PORT=3721 ./your_application
 cd .\Microservices\random_number_service
 start cmd /k cargo run
-cd .\Microservices\lucky_user_service
+cd ..\lucky_user_service
 start cmd /k cargo run
-cd .\Microservices\recmended_movie_service
+cd ..\recmended_movie_service
 start cmd /k cargo run
-echo "All services started"
