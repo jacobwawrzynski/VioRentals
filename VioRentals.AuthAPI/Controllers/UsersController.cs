d3135d290d797c7ec854e5348b12b8195714ff29
Jakub Wawrzyński <jacobwawrzynski@gmail.com>
2023-07-08 18:42:30 +0200

    VIOR-18 Update UserController

--- VioRentals.AuthAPI/Controllers/UsersController.cs
+++ VioRentals.AuthAPI/Controllers/UsersController.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using VioRentals.Core.DTOs;
 using VioRentals.Infrastructure.Repositories.Interfaces;
 
 namespace VioRentals.AuthAPI.Controllers
@@ -24,6 +26,60 @@ namespace VioRentals.AuthAPI.Controllers
             _appSettings = appSettings.Value;
         }
 
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate(LoginDto model)
+        {
+            var response = await _userService.AuthenticateAsync(model);
+            return Ok(response);
+        }
 
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto model)
+        {
+            await _userService.RegisterAsync(model);
+            return Ok(new { message = "Registration successful" });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var users = await _userService.FindAllAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var user = await _userService.FindByIdAsync(id);
+            return Ok(user);
+        }
+
+        // TO DO
+
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> Update(int id, RegisterDto model)
+        //{
+        //    var user = await _userService.FindByIdAsync(id);
+        //    if (user is null)
+        //    {
+        //        return BadRequest(new { message = "User does not exist"});
+        //    }
+        //    await _userService.UpdateUserAsync(user);
+        //    return Ok(new { message = "User updated successfully" });
+        //}
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var user = await _userService.FindByIdAsync(id);
+            if (user is null)
+            {
+                return BadRequest(new { message = "User does not exist" });
+            }
+            await _userService.DeleteUserAsync(user);
+            return Ok(new { message = "User deleted successfully" });
+        }
     }
 }
