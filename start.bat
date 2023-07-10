78bf73df2bc64117813adc05a26b77500258a7a7
KrzysztofJaronczyk <krzysztofjaronczykzyt@gmail.com>
2023-07-10 12:37:17 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -4,3 +4,7 @@ cd ..\lucky_user_service
 start cmd /k cargo run
 cd ..\recmended_movie_service
 start cmd /k cargo run
+
+start microsoft-edge:http://127.0.0.1:8000/random_number/1/100
+start microsoft-edge:http://127.0.0.1:8001/recommend_movie
+start microsoft-edge:http://127.0.0.1:8002/lucky_user
