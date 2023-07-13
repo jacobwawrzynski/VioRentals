2a1381da02e694e8d02969f75fc9fbf4631547e4
Jakub Wawrzyński <jacobwawrzynski@gmail.com>
2023-07-13 15:40:22 +0200

    VIOR-18 Retrieve to using HttpClinet

--- VioRentals.Web/Controllers/AuthenticateController.cs
+++ VioRentals.Web/Controllers/AuthenticateController.cs
@@ -8,14 +8,6 @@ namespace VioRentals.Web.Controllers
 {
     public class AuthenticateController : Controller
     {
-        private readonly HttpClient _httpClient;
-        public AuthenticateController(HttpClient httpClient) 
-        {
-            httpClient = new HttpClient();
-            httpClient.BaseAddress = new Uri("https://localhost:7071/");
-            _httpClient = httpClient;
-        }
-
         // TESTING PURPOSES
         public ActionResult Index()
         {
@@ -26,15 +18,12 @@ namespace VioRentals.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                //using (var client = new HttpClient())
-                //{
-                //    client.BaseAddress = new Uri("https://localhost:7071");
-                //    JsonContent content = JsonContent.Create(loginDto);
-                //    await client.PostAsync("https://localhost:7071/api/Users/login", content);
-                //}
-                
-                JsonContent content = JsonContent.Create(loginDto);
-                await _httpClient.PostAsync("api/Users/login", content);
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://localhost:7071");
+                    JsonContent content = JsonContent.Create(loginDto);
+                    await client.PostAsync("https://localhost:7071/api/Users/login", content);
+                }
 
                 return RedirectToAction("Index");
             }
@@ -45,15 +34,12 @@ namespace VioRentals.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                //using (var client = new HttpClient())
-                //{
-                //    client.BaseAddress = new Uri("https://localhost:7071");
-                //    JsonContent content = JsonContent.Create(registerDto);
-                //    await client.PostAsync("https://localhost:7071/api/Users/register", content);
-                //}
-
-                JsonContent content = JsonContent.Create(registerDto);
-                await _httpClient.PostAsync("api/Users/register", content);
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://localhost:7071");
+                    JsonContent content = JsonContent.Create(registerDto);
+                    await client.PostAsync("https://localhost:7071/api/Users/register", content);
+                }
 
                 return RedirectToAction("Index");
             }
@@ -62,13 +48,11 @@ namespace VioRentals.Web.Controllers
 
         public async Task<ActionResult> GetAll()
         {
-            //using (var client = new HttpClient())
-            //{
-            //    client.BaseAddress = new Uri("https://localhost:7071");
-            //    HttpResponseMessage response = await client.GetAsync("https://localhost:7071/api/Users/all");
-            //}
-
-            var response = await _httpClient.GetAsync("api/Users/all");
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7071");
+                HttpResponseMessage response = await client.GetAsync("https://localhost:7071/api/Users/all");
+            }
 
             return RedirectToAction("Index");
 
